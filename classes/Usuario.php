00b12bf7fea23fa6c1409bb65f18e8c2409a1a65
Moonlight404 <59587363+Moonlight404@users.noreply.github.com>
2020-04-16 20:50:40 -0300

    Add files via upload

---
+++ classes/Usuario.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Classes;
+
+require 'Conexao.php';
+use Classes\Conexao;
+
+class Usuario {
+    
+    private $conexao;
+    
+    public function __construct() {
+        $this->conexao = Conexao::getConexao();
+    }
+    
+    public function listar() {
+        $sql = "select * from usuario;";       
+        $q = $this->conexao->prepare($sql);
+        $q->execute();
+        $users = array();
+        foreach($q as $result){
+            $users[] = $result;
+        };
+        return $users;
+    }
+
+    public function editar($codigo, $nome, $email, $login, $senha){
+        $sql = "update usuario set nome = ?, email = ?, login = ?, senha = ? where codigo = ?;";       
+        $q = $this->conexao->prepare($sql);
+        $q->bindParam(1, $nome);
+        $q->bindParam(2, $email);
+        $q->bindParam(3, $login);
+        $q->bindParam(4, md5($senha));
+        $q->bindParam(5, $codigo);
+        $q->execute();
+    }
+
+    public function delete($codigo){
+        $sql = "delete from usuario where codigo = ?;";       
+        $q = $this->conexao->prepare($sql);
+        $q->bindParam(1, $codigo);
+        $q->execute();
+    }
+    
+    public function inserir($nome, $email, $login, $senha) {
+        $sql = "insert into usuario (nome, email, login, senha) values (?, ?, ?, ?);";
+        $q = $this->conexao->prepare($sql);
+        $q->bindParam(1, $nome);
+        $q->bindParam(2, $email);
+        $q->bindParam(3, $login);
+        $q->bindParam(4, md5($senha));
+        $q->execute();
+    }
+    
+}
+
+?>
---
+++ js/index.js
@@ -0,0 +1,173 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        user: {
+            "nome": "",
+            "email": "",
+            "login": "",
+            "senha": "",
+            "confirma": "",
+        },
+        erros: [],
+        cadastrare: true,
+        excluindo: false,
+        usuarios: [],
+        editando: false,
+        editandoQuem: {},
+        deletandoQuem: {}
+    },
+    methods: {
+        cadastrar(){
+            if(this.user.nome.length == 0 || this.user.email.length == 0 || this.user.login.length == 0 || this.user.senha.length == 0){
+                const erro = {
+                    "id": this.erros.length,
+                    "erro": "Preencha os campos",
+                    "progress": 100
+                }
+                this.erros.push(erro)
+                const interval = setInterval(() => {
+                    const id = this.erros.indexOf(erro)
+                    this.erros[id].progress -= 10;
+                    if(this.erros[id].progress == 0){
+                        clearInterval(interval)
+                        this.erros.splice(id, 1)
+                    }
+                }, 300);
+            }
+            else if(this.user.senha !== this.user.confirma){
+                const erro = {
+                    "id": this.erros.length,
+                    "erro": "Senha diferentes",
+                    "progress": 100
+                }
+                this.erros.push(erro)
+                const interval = setInterval(() => {
+                    const id = this.erros.indexOf(erro)
+                    this.erros[id].progress -= 10;
+                    if(this.erros[id].progress == 0){
+                        clearInterval(interval)
+                        this.erros.splice(id, 1)
+                    }
+                }, 300);
+            } else{
+                $.ajax({
+                    method: 'post',
+                    url: '/api.php?tipo=cadastrar',
+                    data: `nome=${this.user.nome}&email=${this.user.email}&login=${this.user.login}&senha=${this.user.senha}`,
+                    dataType: 'json',
+                    success: function (data) {
+                       
+                    },
+                    error: function (argument) {
+                       
+                    }
+                });
+                this.cadastrare = false
+                if(this.usuarios.length > 0){
+                const codigo = parseInt(this.usuarios[this.usuarios.length - 1].codigo )+ 1
+                this.usuarios.push({
+                    codigo: codigo,
+                    "nome": this.user.nome,
+                    "email": this.user.email,
+                    "login": this.user.login,
+                    "senha":this.user.senha,
+                })
+                } else{
+                    this.listarUsuarios()
+                    this.getListUser()
+                }
+                this.user = {
+                    "nome": "",
+                    "email": "",
+                    "login": "",
+                    "senha": "",
+                    "confirma": "",
+                }
+           
+            }
+            
+        } ,
+        nothing(){
+
+        },
+        getListUser(){
+            setTimeout(() => {
+                this.usuarios = JSON.parse(localStorage.getItem("users"));
+               }, 400);
+        },
+        listarUsuarios(){
+            $.ajax({
+                method: 'get',
+                url: '/api.php?tipo=listar',
+                dataType: 'json',
+                success: function (data) {
+                    localStorage.setItem("users", JSON.stringify(data))
+                },
+                error: function (argument) {
+                   
+                }
+            });
+            this.cadastrare = false
+            this.getListUser()
+        },
+        save(){
+            $.ajax({
+                method: 'post',
+                url: '/api.php?tipo=editar',
+                data: `codigo=${this.editandoQuem.codigo}&nome=${this.editandoQuem.nome}&email=${this.editandoQuem.email}&login=${this.editandoQuem.login}&senha=${this.editandoQuem.senha}`,
+                dataType: 'json',
+                success: function (data) {
+                    localStorage.setItem("users", JSON.stringify(data))
+                },
+                error: function (argument) {
+                   
+                }
+            });
+            this.editandoQuem = {};
+            this.editando = false;
+            $.ajax({
+                method: 'get',
+                url: '/api.php?tipo=listar',
+                dataType: 'json',
+                success: function (data) {
+                    localStorage.setItem("users", JSON.stringify(data))
+                },
+                error: function (argument) {
+                   
+                }
+            });
+            const found = this.usuarios.find(e => e == this.editandoQuem)
+            if(found){
+                const id = this.usuarios.indexOf(found)
+                this.usuarios [id] = this.editandoQuem
+            }
+        },
+        editar(people){
+            this.editandoQuem = people
+            this.editando = true
+        },
+        deletee(){
+            $.ajax({
+                method: 'post',
+                url: '/api.php?tipo=deletar&codigo='+this.deletandoQuem.codigo,
+                dataType: 'json',
+                success: function (data) {
+                    localStorage.setItem("users", JSON.stringify(data))
+                },
+                error: function (argument) {
+                   
+                }
+            });
+            this.excluindo = false
+            const found = this.usuarios.find(e => e == this.deletandoQuem)
+            if(found){
+                const id = this.usuarios.indexOf(found)
+                this.usuarios.splice(id, 1)
+            }
+        },
+        deletar(people){
+            this.excluindo = true
+            this.deletandoQuem = people
+        }
+    }
+})
